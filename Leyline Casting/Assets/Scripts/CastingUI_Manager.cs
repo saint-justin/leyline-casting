ac3f7cba5078837bfbda696f83c16656fa7ca13f
Justin <jtv6445@g.rit.edu>
2019-11-17 22:29:14 -0500

    Made and added power/angle UI elements

---
+++ Leyline Casting/Assets/Scripts/CastingUI_Manager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    // Getting refs to external items
+    public GameObject angleSprite;
+    public GameObject arrowSprite;
+
+    private bool powerMeasured = false;
+    private bool angleMeasuring = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!powerMeasured)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+
+            }
+        }
+
+    }
+
+
+}
