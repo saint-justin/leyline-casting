ef58b23ac1bebdd010b6620341623950172f9758
Justin <jtv6445@g.rit.edu>
2019-11-18 21:47:57 -0500

    Adding scenes back to master. Power and angle indicators complete.

---
+++ Leyline Casting/Assets/Scripts/Camera_Manager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Manager : MonoBehaviour
+{
+    // External References
+    public Camera activeCamera;
+
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+
+    private  float duration;
+    private float timeTotal;
+
+    private bool moving = false;
+
+    public AnimationCurve movementCurve;
+
+    // Start is called once
+    void Start()
+    {
+        activeCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Testing camera movement
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Vector3 newPos = new Vector3(activeCamera.transform.position.x, activeCamera.transform.position.y + 5, activeCamera.transform.position.z);
+            TrackTo(newPos, 1.0f);
+        }
+
+        if (moving)
+            ContinueMoving();
+    }
+
+    /// <summary>
+    /// Use to move the camera to a new position over a given duration
+    /// </summary>
+    /// <param name="_targetPosition"></param>
+    /// <param name="_duration"></param>
+    public void TrackTo(Vector3 _targetPosition, float _duration)
+    {
+        // Make sure we're moving to a new position
+        if (_targetPosition == activeCamera.transform.position)
+            return;
+
+
+        // Set up all variables needed to run continue track
+        duration = _duration;
+        targetPosition = _targetPosition;
+        startPosition = activeCamera.transform.position;
+        timeTotal = 0.0f;
+        moving = true;
+    }
+
+    /// <summary>
+    /// Internal function to keep moving to the target position;
+    /// </summary>
+    private void ContinueMoving()
+    {
+        //  Check if we've hit our destination
+        if (timeTotal >= duration)
+        {
+            activeCamera.transform.position = targetPosition;
+            moving = false;
+            timeTotal = 0.0f;
+        }
+
+        // If not, continue moving toward target position
+        timeTotal += Time.deltaTime;
+        float timePercentage = timeTotal / duration;
+        float movePercentage = movementCurve.Evaluate(timePercentage);
+
+        activeCamera.transform.position = Vector3.Lerp(startPosition, targetPosition, movePercentage);
+    }
+}
---
+++ Leyline Casting/Assets/Scripts/CastingUI_Manager.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+
+public class CastingUI_Manager : MonoBehaviour
+{
+    // Getting refs to external items
+    public GameObject angleBarParent;
+    private GameObject angleBarIndicator;
+    public GameObject powerBarParent;
+    private GameObject powerBarIndicator;
+
+    // Power and power indicator info
+    private bool increasingPower = false;       // Used to denote if the power bar is moving up or down
+    private float powerMod = 1.0f;              // Used with increasingPower to move the power indicator up/down
+    private float currentPower = 0.0f;
+    private float maxPower = 2.0f;
+    private float powerPercentage = -1.0f;      // If this returns -1 something's bust
+
+    private Vector3 powerBarMax;    // Two points for the power bar indicator to move back and forth between
+    private Vector3 powerBarMin;
+
+    // Angle and angle indicator info
+    private float angleMod = -1;     // Denotes the rotation direction (cw vs. ccw) of the angle indicator
+    private float currentAngle = 90.0f;
+    private float maxAngle = 180.0f;
+    private float minAngle = 0.0f;
+
+    private enum fishingState
+    {
+        powering,
+        angling,
+        none
+    }
+
+    private fishingState currentState = fishingState.none;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerBarIndicator = powerBarParent.transform.GetChild(0).gameObject;
+        angleBarIndicator = angleBarParent.transform.GetChild(0).gameObject;
+
+        float heightMax = powerBarParent.GetComponent<SpriteRenderer>().bounds.max.y;
+        float heightMin = powerBarParent.GetComponent<SpriteRenderer>().bounds.min.y;
+
+        powerBarMax = new Vector3(powerBarIndicator.transform.position.x, heightMax, powerBarIndicator.transform.position.z);
+        powerBarMin = new Vector3(powerBarIndicator.transform.position.x, heightMin, powerBarIndicator.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // State machine for casting and casting UI
+        switch (currentState)
+        { 
+            case fishingState.powering:
+                // This section updates the position of the indicator on the screen and underlying values
+                if (currentPower >= maxPower)
+                    powerMod = -1.0f;
+                else if (currentPower <= 0.0f)
+                    powerMod = 1.0f;
+
+                currentPower += Time.deltaTime * powerMod;
+                UpdatePowerBarPos();
+
+                // Checks for the end to powering
+                if (Input.GetMouseButtonDown(0))
+                {
+                    currentState = fishingState.angling;
+
+                    // Disable power bar visuals & enable angle bar visuals
+                    powerBarParent.GetComponent<SpriteRenderer>().enabled = false;
+                    powerBarIndicator.GetComponent<SpriteRenderer>().enabled = false;
+
+                    angleBarParent.GetComponent<SpriteRenderer>().enabled = true;
+                    angleBarIndicator.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().enabled = true;
+
+                    powerPercentage = currentPower / maxPower;
+                }
+                break;
+
+            case fishingState.angling:
+                if (currentAngle >= maxAngle)
+                {
+                    angleMod = -1.0f; 
+                    //Debug.Log("Flip");
+                }
+                else if (currentAngle <= minAngle)
+                {
+                    angleMod = 1.0f; 
+                    //Debug.Log("Flop");
+                }
+
+                currentAngle += Time.deltaTime * 90.0f * angleMod;      // Increase the float in the middle here to increase speed
+                //Debug.Log(currentAngle);
+                
+
+                angleBarIndicator.transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Lerp(-90.0f, 90.0f, currentAngle / 180.0f));
+                Debug.Log(angleBarIndicator.transform.rotation.z);
+                break;
+
+            case fishingState.none:
+                // Checks if we need to start powering state
+                if (Input.GetMouseButtonDown(0))
+                {
+                    currentState = fishingState.powering;
+                    powerBarParent.GetComponent<SpriteRenderer>().enabled = true;
+                    powerBarIndicator.GetComponent<SpriteRenderer>().enabled = true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Function to call each frame the power button is held down
+    /// </summary>
+    void UpdatePowerBarPos()
+    {
+        powerBarIndicator.transform.position = Vector3.Lerp(powerBarMin, powerBarMax, currentPower / maxPower);
+    }
+
+    /// <summary>
+    /// Fxn called when the player releases the pressed button
+    /// </summary>
+    void FullyPowered()
+    {
+
+    }
+}
