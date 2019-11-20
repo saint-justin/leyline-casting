9d91cafa22dc9ec18a016cc85697030d0c334cda
Justin <jtv6445@g.rit.edu>
2019-11-20 08:54:09 -0500

    Added fish assets to project and fish prefab w/ script

---
+++ Leyline Casting/Assets/Scripts/FishMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    public float timeTillFlipFlop = 3.0f;
+    public float fishSpeed = 2.0f;
+    private float directionMod = -1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTillFlipFlop -= Time.deltaTime;
+        if (timeTillFlipFlop <= 0)
+        {
+            directionMod *= -1.0f;
+            timeTillFlipFlop = 3.0f;
+            gameObject.GetComponent<SpriteRenderer>().flipX = !gameObject.GetComponent<SpriteRenderer>().flipX;
+        }
+
+        Vector3 newPosition = gameObject.transform.position;
+        newPosition.x += Time.deltaTime * fishSpeed * directionMod;
+        gameObject.transform.position = newPosition;
+    }
+}
