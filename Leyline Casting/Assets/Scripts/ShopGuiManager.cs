5252f34fa5686b049451dcf600ea57689a714961
Justin <jtv6445@g.rit.edu>
2019-12-02 10:02:24 -0500

    working on menus

---
+++ Leyline Casting/Assets/Scripts/ShopGuiManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopGuiManager : MonoBehaviour
+{
+    public GameObject shopGameObject;
+    private bool shopDisplaying = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            shopGameObject.SetActive(!shopDisplaying);
+        }
+    }
+}
