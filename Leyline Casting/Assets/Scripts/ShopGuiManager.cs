897fc6e3822cb758c78d195ad9b4eefe008967d4
Justin <jtv6445@g.rit.edu>
2019-12-04 10:09:30 -0500

    Added shop toggling

--- Leyline Casting/Assets/Scripts/ShopGuiManager.cs
+++ Leyline Casting/Assets/Scripts/ShopGuiManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ShopGuiManager : MonoBehaviour
 {
     public GameObject shopGameObject;
-    private bool shopDisplaying = false;
+    public GameObject shopGameIcon;
+    private bool shopDisplaying = true;
 
 
     // Update is called once per frame
@@ -13,7 +14,28 @@ public class ShopGuiManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
-            shopGameObject.SetActive(!shopDisplaying);
+            if (shopDisplaying)
+            {
+                Vector3 p = shopGameObject.transform.position;
+                p.x -= 3000;
+                shopGameObject.transform.position = p;
+
+                Vector3 p2 = shopGameIcon.transform.position;
+                p2.x += 3000;
+                shopGameIcon.transform.position = p2;
+            }
+            else
+            {
+                Vector3 p = shopGameObject.transform.position;
+                p.x += 3000;
+                shopGameObject.transform.position = p;
+
+                Vector3 p2 = shopGameIcon.transform.position;
+                p2.x -= 3000;
+                shopGameIcon.transform.position = p2;
+            }
+
+            shopDisplaying = !shopDisplaying;
         }
     }
 }
