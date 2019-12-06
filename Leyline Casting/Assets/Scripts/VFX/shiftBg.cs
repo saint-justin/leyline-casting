ba88579525f075f6b1792022f68b6519a589649a
Justin <jtv6445@g.rit.edu>
2019-12-06 10:53:19 -0500

    Working on fishmonger file extended

---
+++ Leyline Casting/Assets/Scripts/VFX/shiftBg.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shiftBg : MonoBehaviour
+{
+    float startTime;
+    float currentTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        Vector3 pos = this.gameObject.transform.position;
+        pos.x += Mathf.Sin(currentTime) * 0.01f;
+        this.gameObject.transform.position = pos;
+    }
+}
