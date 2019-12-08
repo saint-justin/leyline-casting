a3b6e515f2621a733f2872d5a2c6849dbfd70301
Andrew Schwartz <andrewschwartz99@gmail.com>
2019-12-08 17:27:33 -0500

    added changing scenes to menus (and build settings)

---
+++ Leyline Casting/Assets/Scripts/MenuMethods.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMethods : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // loads scene specified by scene name
+    // sceneName is the scene filename minus the extension (no ".unity")
+    public void ChangeScene(string sceneName)
+    {
+        // if no scene name specified
+        if (sceneName == "" || sceneName == null)
+            return; // do nothing
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
