8182876c9e826da165512c2c2a879ca08d503262
Justin <jtv6445@g.rit.edu>
2019-12-16 01:28:53 -0500

    Updating VFX, Fishmonger File UI Laid Out

--- Leyline Casting/Assets/Scripts/VFX/fishReeled.cs
+++ Leyline Casting/Assets/Scripts/VFX/fishReeled.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class fishReeled : MonoBehaviour
 {
     // Prototype to manipulate
+    public GameObject particles;
     public GameObject moneyText;
-    private Queue<GameObject> instantiatedObjects;
+
+    private List<GameObject> instantiatedObjects;
+    private Queue<GameObject> destructionQueue;
     private Queue<float> objectDeaths;
+
     private bool running = false;           // Whether or not the script is currently running
     private float floatingSpeed = 2.0f;     // Speed at which the text rises
     private float lifeTime = 1.0f;          // Lifetime of each text object
 
-    public ParticleSystem particles;
-
     void Start()
     {
-        instantiatedObjects = new Queue<GameObject>();
+        instantiatedObjects = new List<GameObject>();
+        destructionQueue = new Queue<GameObject>();
         objectDeaths = new Queue<float>();
     }
 
@@ -26,7 +29,8 @@ public class fishReeled : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.O))
         {
             Begin("200", new Vector3(0, 0, 0));
-            Debug.Log("NEW VFX");
+            Debug.Log(Time.time);
+            Debug.Log(Time.time + lifeTime);
         }
 
         if (running)
@@ -41,15 +45,18 @@ public class fishReeled : MonoBehaviour
     public void Begin(string _input, Vector3 _startPos)
     {
         // Spawn in the new text object
-        GameObject instantiatedObject = Instantiate(moneyText, this.transform.position, Quaternion.identity);
-        instantiatedObjects.Enqueue(instantiatedObject);
-        objectDeaths.Enqueue(Time.time + lifeTime);
+        GameObject tempCash = moneyText;
+        GameObject tempSystem = particles;
 
-        Debug.Log("Object instantiated...");
+        GameObject instantiatedObject = Instantiate(moneyText, this.transform);
+        GameObject instantiatedSystem = Instantiate(particles, instantiatedObject.transform);
+
         Debug.Log(instantiatedObject);
 
-        // Run the particle system
-        particles.Play();
+        instantiatedObjects.Add(instantiatedObject);
+        destructionQueue.Enqueue(instantiatedObject);
+
+        Invoke("DestroyObject", lifeTime);
 
         // Start the animating fxn
         running = true;
@@ -64,25 +71,19 @@ public class fishReeled : MonoBehaviour
         // Update every item in instantiatedObjects to float up
         for(int i=0; i<instantiatedObjects.Count; i++)
         {
-            GameObject obj = instantiatedObjects.Dequeue();
-            float life = objectDeaths.Dequeue();
-
-            // Update lifetime of the object & check if it's expired
-            if (life <= Time.time)
-            {
-                Destroy(obj);
-                Debug.Log("Destroying text object...");
-                continue;
-            }
-
             // Update the position of the object if it's still got time
-            Vector3 pos = obj.transform.position;
+            Vector3 pos = instantiatedObjects[i].transform.position;
             pos.y += floatingSpeed;
             pos.x += Mathf.Sin(Time.deltaTime);
-            obj.transform.position = pos;
-
-            instantiatedObjects.Enqueue(obj);
-            objectDeaths.Enqueue(life);
+            instantiatedObjects[i].transform.position = pos;
         }
+    }
+
+    private void DestroyObject()
+    {
+        GameObject obj = destructionQueue.Dequeue();
+
+        instantiatedObjects.Remove(obj);
+        Destroy(obj);
     }
 }
