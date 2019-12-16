2a34fac54e9be46067c869e1d854dad7f8b95e17
Justin <jtv6445@g.rit.edu>
2019-12-15 22:18:30 -0500

    Shop and back-to-game buttons clickable

---
+++ Leyline Casting/Assets/Scripts/LaunchShop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchShop : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject GuiGO;
+    private ShopGuiManager GuiManager;
+
+    void Start()
+    {
+        GuiManager = GuiGO.GetComponent<ShopGuiManager>();
+    }
+
+    public void ClickEvent()
+    {
+        if (GamePaused)
+            Resume();
+        else
+            Pause();
+    }
+
+    /// <summary>
+    ///  Pauses the game and launches pause menu
+    /// </summary>
+    void Pause()
+    {
+        GamePaused = !GamePaused;
+        GuiManager.ToggleDisplay();
+        Time.timeScale = 0.0f;
+    }
+
+    /// <summary>
+    ///  Resumes the game and hides pause menu
+    /// </summary>
+    void Resume()
+    {
+        GamePaused = !GamePaused;
+        GuiManager.ToggleDisplay();
+        Time.timeScale = 1.0f;
+    }
+}
--- Leyline Casting/Assets/Scripts/ShopGuiManager.cs
+++ Leyline Casting/Assets/Scripts/ShopGuiManager.cs
@@ -9,33 +9,29 @@ public class ShopGuiManager : MonoBehaviour
     private bool shopDisplaying = true;
 
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleDisplay()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if (shopDisplaying)
         {
-            if (shopDisplaying)
-            {
-                Vector3 p = shopGameObject.transform.position;
-                p.x -= 3000;
-                shopGameObject.transform.position = p;
+            Vector3 p = shopGameObject.transform.position;
+            p.x -= 3000;
+            shopGameObject.transform.position = p;
 
-                Vector3 p2 = shopGameIcon.transform.position;
-                p2.x += 3000;
-                shopGameIcon.transform.position = p2;
-            }
-            else
-            {
-                Vector3 p = shopGameObject.transform.position;
-                p.x += 3000;
-                shopGameObject.transform.position = p;
-
-                Vector3 p2 = shopGameIcon.transform.position;
-                p2.x -= 3000;
-                shopGameIcon.transform.position = p2;
-            }
-
-            shopDisplaying = !shopDisplaying;
+            Vector3 p2 = shopGameIcon.transform.position;
+            p2.x += 3000;
+            shopGameIcon.transform.position = p2;
         }
+        else
+        {
+            Vector3 p = shopGameObject.transform.position;
+            p.x += 3000;
+            shopGameObject.transform.position = p;
+
+            Vector3 p2 = shopGameIcon.transform.position;
+            p2.x -= 3000;
+            shopGameIcon.transform.position = p2;
+        }
+
+        shopDisplaying = !shopDisplaying;
     }
 }
